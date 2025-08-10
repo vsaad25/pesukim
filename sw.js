1490dea9a1648c0828657b43e160ff29546133dd
vsaad25 <91304236+vsaad25@users.noreply.github.com>
2025-08-10 01:37:40 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "pesukim-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
